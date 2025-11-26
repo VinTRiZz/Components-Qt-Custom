89ae6313758fa841ba4b78019069dfc23268cb51
VinTRiZz <andzig501@gmail.com>
2025-11-26 21:31:21 +0300

    Расширил контекстное меню

--- src/ObjectView/ovcontextmenulayer.hpp
+++ src/ObjectView/ovcontextmenulayer.hpp
@@ -6,6 +6,8 @@
 #include "ovcontextmenulayer.hpp"
 #include "ovinternalscene.hpp"
 
+#include "../ObjectItems/basicitem.hpp"
+
 #include <QContextMenuEvent>
 
 #include <QAction>
@@ -17,6 +19,10 @@ class OVContextMenuLayer
 {
 public:
     OVContextMenuLayer() {
+        m_subContextMenu = new QMenu("Объект", static_cast<BaseView*>(this));
+        m_mainContextMenu.addMenu(m_subContextMenu);
+        m_subContextMenu->setEnabled(false);
+
         auto pGridAction = m_mainContextMenu.addAction("Сетка", [this]() {
             static_cast<BaseView*>(this)->getScene()->setGridEnabled(!static_cast<BaseView*>(this)->getScene()->getIsGridEnabled());
             for (auto* pAction : m_mainContextMenu.actions()) {
@@ -52,6 +58,20 @@ public:
         });
     }
 
+    void addContextMenuAction(const QString& text, const std::function<void(QGraphicsItem*)>& processor) {
+        auto pAction = new QAction(text);
+        connect(pAction, &QAction::triggered,
+                pAction, [this, processor](){
+            processor(m_contextMenuItem);
+        });
+        m_subContextMenu->addAction(pAction);
+    }
+
+    void addSubmenu(const QString& text, const std::function<bool(QGraphicsItem*)>& activator) {
+        auto pMenu = new QMenu(text, static_cast<BaseView*>(this));
+        m_mainContextMenu.addMenu(pMenu);
+    }
+
 private:
     QMenu m_mainContextMenu;  //! Основное контекстное меню
     QAction* m_opacityAction{
@@ -62,6 +82,10 @@ private:
         nullptr};  //! Объект, который находился под указателем мыши во время
                    //! вызова контекстного меню
 
+    QMenu* m_subContextMenu; //! Меню с названием "Контекст", которое вызывается для айтемов под курсором
+
+    std::map<QMenu*, std::function<bool(QGraphicsItem*)> > m_submenus; //! Меню, добавленные через addSubmenu
+
 protected:
     void executeContextMenu(QContextMenuEvent* e) {
         auto pHoverItem = static_cast<BaseView*>(this)->getTopItem(e->pos());
@@ -71,9 +95,18 @@ protected:
         } else {
             m_opacityAction->setEnabled(false);
         }
+        m_contextMenuItem = pHoverItem;
+        m_subContextMenu->setEnabled(dynamic_cast<ObjectItems::BasicItem*>(m_contextMenuItem) != nullptr);
+
+        for (auto& [pMenu, activator] : m_submenus) {
+            pMenu->setEnabled(activator(m_contextMenuItem));
+        }
 
         // Подразумевается, что это меню не было определено
         m_mainContextMenu.exec(e->globalPos());
+
+        // Во избежание подвешенных состояний
+        m_contextMenuItem = nullptr;
     }
 };
 
